9c1da473f16554a5ee4661381680431dda3deb0b
Roy van der Meij <rvdmeij@2li.nl>
2022-10-16 23:11:07 +0200

    Disabled WIP header API-Token

--- src/utils/Http.ts
+++ src/utils/Http.ts
@@ -1,17 +1,10 @@
 import axios, { AxiosInstance } from 'axios';
 
-let token = null;
-const ls = window.localStorage;
-if (ls.getItem('token')) {
-  token = ls.getItem('token') ?? '0';
-}
-
 const apiClient: AxiosInstance = axios.create({
     baseURL: process.env.VUE_APP_VAULTI_SERVER,
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        'X-Token': token
     }
 });
 
